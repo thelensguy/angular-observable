29ea553718c464042634947ff3f1effb2ab4a317
Than Nguyenvu <thelensguy@gmail.com>
2020-07-09 14:18:03 -0700

    Errors and Completions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,12 +19,27 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is greater 3!"));
+        }
         count++;
       }, 1000);
     });
-    this.firstObserSubscription = customIntervalObersvable.subscribe((data) => {
-      console.log(data);
-    });
+    this.firstObserSubscription = customIntervalObersvable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log("Completed!");
+      }
+    );
   }
 
   ngOnDestroy() {
